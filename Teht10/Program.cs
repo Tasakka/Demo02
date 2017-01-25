2216df9c3a03ff19888eb91f74f8569f01d93320
k3236 <k3236@student.jamk.fi>
2017-01-25 13:53:15 +0200

    ei ehkä paras ratkaisu

---
+++ Teht10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arvot = { 1, 2, 33, 44, 55, 68, 77, 96, 100 };
+            int n = 0;
+            while (n < arvot.Length)
+            {
+                if (n % 2 ==0)
+                {
+                    Console.Write("HEP!!! \n");
+                    n++;
+                    continue;
+                }
+                else
+                {
+                    n++;
+                    continue;
+                }
+            }
+
+        }
+    }
+}
