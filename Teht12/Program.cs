60d36502b6270fd632d87d6c8786675f32796c09
Juuso <k3236@student.jamk.fi>
2017-01-25 17:23:47 +0200

    toimii

---
+++ Teht12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+            int [] luvut = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Anna luku > ");
+                luku = int.Parse(Console.ReadLine());
+                luvut[i] = luku;
+            }
+            Array.Reverse(luvut);
+            Console.WriteLine(luvut[0]);
+            Console.WriteLine(luvut[1]);
+            Console.WriteLine(luvut[2]);
+            Console.WriteLine(luvut[3]);
+            Console.WriteLine(luvut[4]);
+        }
+    }
+}
