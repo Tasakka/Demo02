0d63c045907c1fc55dd1bc531edeb69421d62e53
Juuso <k3236@student.jamk.fi>
2017-01-25 20:45:58 +0200

    jipii

---
+++ Teht18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna jokin merkkijono > ");
+            string mj = Console.ReadLine();
+            char[] merkkijono = mj.ToCharArray();
+            Array.Reverse(merkkijono);
+            string käännös = new string(merkkijono);
+            Console.WriteLine(käännös);
+            if (käännös == mj)
+            {
+                Console.WriteLine("Tämähän on palindromi!");
+            }
+            else
+            {
+                Console.WriteLine("Sana on nyt väärinpäin!");
+            }
+        }
+    }
+}
